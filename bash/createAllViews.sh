f8356f0c86d8c8d586cb803cc0147dd8b229eec7
m <m@m.com>
2020-12-11 16:05:18 -0500

    successfully referencing the ".env" file to import bash configuraiton values

--- bash/createAllViews.sh
+++ bash/createAllViews.sh
@@ -2,7 +2,7 @@
 
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )
 # shellcheck disable=SC1090
-source "$PARENT_PATH"/config.sh;
+source "$PARENT_PATH"/../node/.env;
 
 # shellcheck disable=SC1090
 source "$PARENT_PATH"/run_mongo_commands_from_folder.sh;
--- bash/run_mongo_commands_from_folder.sh
+++ bash/run_mongo_commands_from_folder.sh
@@ -3,7 +3,7 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )
 
 # shellcheck disable=SC1090
-source "$PARENT_PATH"/config.sh;
+source "$PARENT_PATH"/../node/.env;
 
 # shellcheck disable=SC1090
 source "$PARENT_PATH"/current_date_time.sh;
