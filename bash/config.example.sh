b4f6daa4378d74fb37162eab2e05a7c323a6dd21
m <m@m.com>
2020-12-11 15:30:09 -0500

    creating a sample config file

---
+++ bash/config.example.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# shellcheck disable=SC2034
+MONGODB_CREATE_VIEWS_FOLDER="../mongodb/views"
+MONGODB_CREATE_INDICES_FOLDER="../mongodb/indices"
+MONGO_IP='localhost';
+MONGO_PORT='27017';
+MONGO_DATABASE='scores';
+MONGO_USERNAME='[username here]';
+MONGO_PASSWORD='[password here]';
+MONGO_AUTHDB='admin';
+
+SYSTEM_NAME_MAC="Darwin"
+SYSTEM_NAME_LINUX="Linux"
