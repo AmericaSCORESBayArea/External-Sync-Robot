96616d8bc5d9447023b9093cc35ac3d46d8a0028
m <m@m.com>
2020-12-15 14:48:10 -0500

    adding logic for initializing indices and views when the database is initialized

--- bash/server/dev/startup_dev.sh
+++ bash/server/dev/startup_dev.sh
@@ -10,20 +10,38 @@ MONGODB_ROOT_FOLDER="db"
 MONGODB_LOG_FOLDER="$MONGODB_ROOT_FOLDER/log"
 MONGODB_DATA_FOLDER="$MONGODB_ROOT_FOLDER/data"
 
+BL_NEEDED_FOLDER_UPDATE=0
+
 if [ ! -d "$MONGODB_ROOT_FOLDER" ]; then
+  BL_NEEDED_FOLDER_UPDATE=1
   echo "Initializing MongoDB Root Folder: $MONGODB_ROOT_FOLDER"
   mkdir -p "$MONGODB_ROOT_FOLDER"
 fi
 if [ ! -d "$MONGODB_LOG_FOLDER" ]; then
+  BL_NEEDED_FOLDER_UPDATE=1
   echo "Initializing MongoDB Log Folder: $MONGODB_LOG_FOLDER"
   mkdir -p "$MONGODB_LOG_FOLDER"
 fi
 if [ ! -d "$MONGODB_DATA_FOLDER" ]; then
+  BL_NEEDED_FOLDER_UPDATE=1
   echo "Initializing MongoDB Data Folder: $MONGODB_DATA_FOLDER"
   mkdir -p "$MONGODB_DATA_FOLDER"
+
 fi
 
 (mongod --config mongod.conf) &
+
+if (( BL_NEEDED_FOLDER_UPDATE == 1 )); then
+
+  echo "Initializing MongoDB Indices and Views..."
+
+  #creates indices
+  ./createAllIndices.sh
+
+  #creates views
+  ./createAllViews.sh
+
+fi
 
 cd ../../ || exit
 
