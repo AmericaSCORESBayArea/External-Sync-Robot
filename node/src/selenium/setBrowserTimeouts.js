3c22cdf95fe215813d452744db105c66048f3d39
m <m@m.com>
2020-12-26 13:20:27 -0500

    adding a selenium module to override the default timeouts - this is required for long-running browser scripts to NOT close the selenium browser and cause errors because they took too long to complete

---
+++ node/src/selenium/setBrowserTimeouts.js
@@ -0,0 +1,20 @@
+const implicit_timeout_milliseconds = 10000;
+const pageLoad_timeout_milliseconds = 10000;
+const script_timeout_milliseconds = 60*60*1000*5;   //5 hours
+const timeoutObj = { implicit: implicit_timeout_milliseconds, pageLoad:pageLoad_timeout_milliseconds, script: script_timeout_milliseconds };
+
+const setBrowserTimeouts = (browser) => {
+  console.log(`setting browser timeouts to : ${timeoutObj}`);
+  return new Promise(async (resolve) => {
+    try {
+      await browser.manage().setTimeouts(timeoutObj)
+      resolve(true);
+    } catch(e) {
+      console.error("error setting browser timeouts");
+      console.error(e);
+      resolve(false);
+    }
+  });
+};
+
+export default setBrowserTimeouts;
