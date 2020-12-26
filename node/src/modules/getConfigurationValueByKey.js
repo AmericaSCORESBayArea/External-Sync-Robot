b535a2ca945b34354e7d05b63c71a8e2c37896bc
m <m@m.com>
2020-12-26 13:09:48 -0500

    creating a module to standardize how configuration values are extracted from the ".env" file

---
+++ node/src/modules/getConfigurationValueByKey.js
@@ -0,0 +1,3 @@
+import configuration from "../config";
+const getConfigurationValueByKey = (configKey) => !!configuration && !!configuration.parsed ? configuration.parsed[configKey] : null;
+export default getConfigurationValueByKey;
