30397d537d56c212308991be37eb8f08e39759a0
m <m@m.com>
2020-12-26 13:23:26 -0500

    creating a module to get the text content from a file within the current project's folder

---
+++ node/src/modules/getTextFileContent.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import getConfigurationValueByKey from "./getConfigurationValueByKey";
+
+const scriptRootFolder = getConfigurationValueByKey("BROWSER_SCRIPT_RELATIVE_FOLDER_ROOT");
+
+const getTextFileContent = (relativeScriptFolderFilePath) => {
+  return new Promise(async (resolve) => {
+    try {
+      const scriptFileAbsoluteLocation = `${scriptRootFolder}/${relativeScriptFolderFilePath}`;
+      console.log(`....getting the script content : ${scriptFileAbsoluteLocation}`);
+      const fileContent = await new Promise(async (resolve, reject) => fs.readFile(scriptFileAbsoluteLocation, "utf8", (err, data) => !!err ? reject(err) : resolve(data)));
+      resolve(fileContent);
+    } catch(e) {
+      console.error(`error reading script file content`);
+      console.error(e);
+      resolve(null);
+    }
+  });
+};
+
+export default getTextFileContent;
