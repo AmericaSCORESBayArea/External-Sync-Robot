200f1d359d2560985d0d5dc0f1fecab3428cb6f1
m <m@m.com>
2020-12-26 13:03:04 -0500

    consolidating all District Participants views

---
+++ mongodb/views/district/district_participants_view.js
@@ -0,0 +1,27 @@
+db.district_participants_view.drop();
+db.createView("district_participants_view","district_participants",
+
+  // Pipeline
+  [
+    // Stage 1
+    {
+      $project: {
+        "_id" : {
+          "$concat" : [
+            "$district",
+            "_",
+            "$participant.id"
+          ]
+        },
+        "participant" : 1.0,
+        "browserDate" : 1.0,
+        "instanceDate" : 1.0,
+        "district" : 1.0,
+        "formValues" : {
+          "$arrayToObject" : "$formValues"
+        }
+      }
+    }
+  ]
+
+);
