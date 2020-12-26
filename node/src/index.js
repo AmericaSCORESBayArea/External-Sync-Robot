4ee2f3a5147e1c2e350bf427fb354177ffbf0433
m <m@m.com>
2020-12-26 13:17:45 -0500

    updating the main entry point for running commands

--- node/src/index.js
+++ node/src/index.js
@@ -1,29 +1,50 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import compare_coach_data from "./mulesoft_api/compare_coach_data";
+import get_coach_data from "./mulesoft_api/get_coach_data";
+import runBrowserScriptFromFile from "./selenium/runBrowserScriptFromFile";
 import {establishDatabaseConnection} from "./mongo/establishDatabaseConnection";
 import getCLIArguments from "./modules/getCLIArguments";
 import closeDatabaseConnection from "./mongo/closeDatabaseConnection";
+import generateAvailableCommandsString from "./modules/generateAvailableCommandsString";
+import get_participant_data from "./mulesoft_api/get_participant_data";
+import list_participants_in_district_not_in_salesforce from "./mulesoft_api/list_participants_in_district_not_in_salesforce";
+import post_new_participants_to_salesforce from "./mulesoft_api/post_new_participants_to_salesforce";
 
 const cliArguments = getCLIArguments();
 
 const availableCommands = [
   {
-    name: "compare",
-    entryPoint: compare_coach_data
+    name: "scrape",
+    entryPoint: runBrowserScriptFromFile
+  },
+  {
+    name: "get_participant_data",
+    entryPoint: get_participant_data
+  },
+  {
+    name: "get_coach_data",
+    entryPoint: get_coach_data
+  },
+  {
+    name: "list_participants_in_district_not_in_salesforce",
+    entryPoint: list_participants_in_district_not_in_salesforce
+  },
+  {
+    name: "post_new_participants_to_salesforce",
+    entryPoint: post_new_participants_to_salesforce
   }
 ];
 
 //main entry point
 const main = () => {
   console.log(`Started : ${new Date()}`);
-  console.log(`....CLI Arguments : ${cliArguments.join(",")}`);
+  console.log(`....CLI Arguments : ${cliArguments.join(", ")}`);
   if (cliArguments.length > 0) {
     const requestedCommand = cliArguments[0];
     const matchingEntryPoint = availableCommands.filter((item) => !!item.name && item.name === requestedCommand);
     if (matchingEntryPoint.length > 0) {
       new Promise(async () => {
-        await establishDatabaseConnection(matchingEntryPoint[0].entryPoint)
+        await establishDatabaseConnection(matchingEntryPoint[0].entryPoint,cliArguments)
           .then((res, err) => {
             if (!!err) {
               console.error("unknown error--1 in main");
@@ -34,6 +55,7 @@ const main = () => {
             console.error(err);
           }).then(async () => {
             await closeDatabaseConnection();
+            console.log(`Done : ${new Date()}`);
           });
       }).then((res, err) => {
         if (!!err) {
@@ -43,16 +65,14 @@ const main = () => {
       }).catch((err) => {
         console.error("unknown error--4 in main");
         console.error(err);
-      }).then(() => {
-        console.log(`Done : ${new Date()}`);
-      });
+      }).then(() => {});
     } else {
       console.log(`no matching command for the request : ${requestedCommand}`);
-      console.log(`available commands : ${availableCommands.map((item, index) => `
-    [${index + 1}] ${item.name}`).join()}`);
+      console.log(generateAvailableCommandsString(availableCommands));
     }
   } else {
     console.log("must pass in at least one argument");
+    console.log(generateAvailableCommandsString(availableCommands));
   }
 };
 
