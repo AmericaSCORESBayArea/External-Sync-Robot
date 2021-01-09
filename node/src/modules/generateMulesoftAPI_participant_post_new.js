67832cf184f60ea17299f6567776bbe229e2e97f
m <m@m.com>
2021-01-09 11:55:31 -0500

    correcting the endpoint to "/contacts"

--- node/src/modules/generateMulesoftAPI_participant_post_new.js
+++ node/src/modules/generateMulesoftAPI_participant_post_new.js
@@ -1,3 +1,3 @@
 import getConfigurationValueByKey from "./getConfigurationValueByKey";
-const generateMulesoftAPI_participant_post_new = () => `${getConfigurationValueByKey("MULESOFT_API_ROOT_URL")}/contact`;
+const generateMulesoftAPI_participant_post_new = () => `${getConfigurationValueByKey("MULESOFT_API_ROOT_URL")}/contacts`;
 export default generateMulesoftAPI_participant_post_new;
