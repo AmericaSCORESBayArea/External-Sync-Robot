becf27dab1cd40cec30a3b0da88e3591bed5f36c
m <m@m.com>
2021-01-09 12:03:08 -0500

    successfully connecting to mongodb and then running insert command instead of importing global mongodb connection

--- node/src/mongo/insertOne.js
+++ node/src/mongo/insertOne.js
@@ -1,9 +1,17 @@
-import createCollectionObject from "./createCollectionObject";
+import getConfigurationValueByKey from "../modules/getConfigurationValueByKey";
+
+const MongoClient = require('mongodb').MongoClient;
+import generateMongoDBConnectionURL from "./establishDatabaseConnection";
 
 const insertOneDocument = (collectionName,document) => {
   try {
-    const collectionObj = createCollectionObject(collectionName);
-    return new Promise((resolve, reject) => collectionObj.insertOne(document, (err, result) => !!err ? reject(err) : resolve(result.insertedId)));
+    return new Promise(async (resolve, reject) => {
+      await MongoClient.connect(generateMongoDBConnectionURL(), {useUnifiedTopology: true}, function (err, client) {
+        const db = client.db(getConfigurationValueByKey("MONGO_DATABASE"));
+        const collection = db.collection(collectionName);
+        collection.insertOne(document, (err, result) => !!err ? reject(err) : resolve(result.insertedId));
+      });
+    });
   } catch (err) {
     console.error(`error inserting (insertOneDocument) into mongo server ${collectionName}`,err);
     return null;
