8b7dcf2f81e99b6815ef003c3a3c8611bc70014a
m <m@m.com>
2021-01-09 12:02:43 -0500

    successfully connecting to mongodb and then running query instead of importing global mongodb connection

--- node/src/mongo/query.js
+++ node/src/mongo/query.js
@@ -1,24 +1,30 @@
-import createCollectionObject from "./createCollectionObject";
+import getConfigurationValueByKey from "../modules/getConfigurationValueByKey";
+
+const MongoClient = require('mongodb').MongoClient;
+import generateMongoDBConnectionURL from "./establishDatabaseConnection";
 
 const queryDocuments = (collectionName,query,fields,limit,skip) => {
   try {
-    const collectionObj = createCollectionObject(collectionName);
-    let options = {};
-    if (!!fields) {
-      options.projection = fields;
-    }
-    if (!!limit || limit === 0) {
-      options.limit = limit;
-    }
-    if (!!skip || skip === 0) {
-      options.skip = skip;
-    }
-    return new Promise((resolve, reject) => {
-      collectionObj.find(query, options, async (err, res) => !!err ? reject(err) : resolve(await res.toArray()));
+    return new Promise(async (resolve, reject) => {
+      await MongoClient.connect(generateMongoDBConnectionURL(), {useUnifiedTopology: true}, function (err, client) {
+        const db = client.db(getConfigurationValueByKey("MONGO_DATABASE"));
+        const collection = db.collection(collectionName);
+        let options = {};
+        if (!!fields) {
+          options.projection = fields;
+        }
+        if (!!limit || limit === 0) {
+          options.limit = limit;
+        }
+        if (!!skip || skip === 0) {
+          options.skip = skip;
+        }
+        collection.find(query, options, async (err, res) => !!err ? reject(err) : resolve(await res.toArray()));
+      });
     });
   } catch (err) {
-    console.error(`error querying mongo server ${collectionName}`,err);
-    console.error(`....requested query`,query);
+    console.error(`error querying mongo server ${collectionName}`, err);
+    console.error(`....requested query`, query);
     return null;
   }
 };
