ac74fc80c73fba4e49541f362f1b549f7f6a2ec7
m <m@m.com>
2021-01-09 13:21:58 -0500

    adding a view to summarize the responses from MuleSoft "/contacts" API

---
+++ mongodb/views/mulesoft_api/mulesoft_api_responses_participants_view.js
@@ -0,0 +1,52 @@
+db.mulesoft_api_responses_participants_view.drop();
+db.createView("mulesoft_api_responses_participants_view","mulesoft_api_responses",
+
+  // Pipeline
+  [
+    // Stage 1
+    {
+      $match: {
+        "requestType" : {
+          "$regex" : "/api/contacts"
+        }
+      }
+    },
+
+    // Stage 2
+    {
+      $unwind: {
+        "path" : "$data"
+      }
+    },
+
+    // Stage 3
+    {
+      $project: {
+        "_id" : {
+          "$concat" : [
+            "$parameters.district",
+            "_",
+            "$parameters.participantId",
+            "_",
+            "$data.Id"
+          ]
+        },
+        "httpAPIRequestId" : "$_id",
+        "httpAPIrequestDate" : 1.0,
+        "dateOfBirth_district" : "$parameters.participantDateOfBirth",
+        "lastNameFirstName_district" : "$parameters.participantName",
+        "participantId_district" : "$parameters.participantId",
+        "district" : "$parameters.district",
+        "participantId_salesForce" : "$data.Id",
+        "firstName_salesforce" : "$data.FirstName",
+        "lastName_salesforce" : "$data.LastName",
+        "dateOfBirth_salesForce" : "$data.DateOfBirth",
+        "zip_salesForce" : "$data.Zip",
+        "homeLanguage_salesForce" : "$data.HomeLanguage",
+        "ethnicity_salesForce" : "$data.Ethnicity",
+        "gender_salesForce" : "$data.Gender",
+        "schoolAttending_salesForce" : "$data.SchoolAttending"
+      }
+    },
+  ]
+);
