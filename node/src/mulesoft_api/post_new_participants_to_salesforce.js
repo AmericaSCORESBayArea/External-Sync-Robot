abcdbe91a293a9ecd9493a600f3673c1c049e2c4
m <m@m.com>
2021-01-09 11:54:26 -0500

    successfully running MuleSoft API new Participant POST request by first setting a default object and then setting fields as needed

--- node/src/mulesoft_api/post_new_participants_to_salesforce.js
+++ node/src/mulesoft_api/post_new_participants_to_salesforce.js
@@ -1,7 +1,4 @@
 import queryDocuments from "../mongo/query";
-import generateMulesoftAPIEndpoint_coach_coachIdAndDate from "../modules/generateMulesoftAPI_coach_coachIdAndDate";
-import getConfigurationValueByKey from "../modules/getConfigurationValueByKey";
-import runMulesoftAPIRequest_GET from "../modules/runMulesoftAPIRequest_GET";
 import runMulesoftAPIRequest_POST from "../modules/runMulesoftAPIRequest_POST";
 import generateMulesoftAPI_participant_post_new from "../modules/generateMulesoftAPI_participant_post_new";
 
@@ -11,104 +8,90 @@ const districtToSalesforceExtraFieldMapping = {
   "Date of Birth" : "Birthdate",
 };
 
-const salesforceEmptyFieldMappings = [
-  "OtherLang",
-  "Emergency_Contact_Phone3",
-  "Gender",
-  "Emergency_Contact_Name",
-  "ParentHomeLang",
-  "Second_Emergency_Contact_Phone1",
-  "Relationship",
-  "ParentPhone1",
-  "Emergency_Contact_Phone2",
-  "ParentLName",
-  "LiabilityWaiver",
-  "Ethnicity",
-  "Second_Emergency_Contact_Relationship",
-  "SchoolName",
-  "PermissiontoCommuteAlone",
-  "MailingStreet",
-  "Volunteer",
-  "Grade",
-  "Second_Emergency_Contact_Name",
-  "DataReleaseWaiver",
-  "MailingCity",
-  "MediaReleaseWaiver",
-  "ParentEnglishFluency",
-  "Second_Emergency_Contact_Phone3",
-  "MailingCountry",
-  "ParentPhone3",
-  "Emergency_Contact_Phone1",
-  "ReducedPriceLunch",
-  "Allergies",
-  "ContactRecordType",
-  "Second_Emergency_Contact_Permission_to_Pickup_child",
-  "Emergency_Contact_Relationship",
-  "ParentEmail",
-  "Emergency_Contact_Permission_to_Pickup_child",
-  "ParentFName",
-  "ParentPhone2",
-  "PersonalEmail",
-  "MailingState",
-  "Second_Emergency_Contact_Phone2",
-  "MiddleName",
-  "HomePhone"
-];
-
 const post_new_participants_to_salesforce = async () => {
   const requestDate = new Date();
   return await new Promise(async (resolve, reject) => {
     try {
       console.log(`Starting Post New Participants Command...`);
-      const missingParticipants = await queryDocuments(`district_participants_not_in_salesforce_view`, {},null,1);
+      const missingParticipants = await queryDocuments(`mulesoft_api_responses_participants_not_found_view`, {});
       if (!!missingParticipants && missingParticipants.length > 0) {
-        console.log(`Count : ${missingParticipants.length} Missing District Participants in Salesforce...`);
+        console.log(`Count : ${missingParticipants.length} Participants to add to Salesforce...`);
         resolve(Promise.all(missingParticipants.map(async (item, index) => {
-          return new Promise((resolve_2,reject_2) => {
+          return new Promise((resolve_2, reject_2) => {
             setTimeout(async () => {
               console.log(`Running Post for ${index + 1} - ${item.participant}`);
               if (!!item.formValues) {
                 const fullNameSplit = item.participant.split(",").map((item) => `${item.trim()}`);
                 if (fullNameSplit.length === 2) {
-                  const FirstName = fullNameSplit[1];
-                  const LastName = fullNameSplit[0];
                   const postRequestFields = {
-                    FirstName,
-                    LastName
+                    "OtherLang": "",
+                    "Emergency_Contact_Phone3": "",
+                    "Gender": "",
+                    "Emergency_Contact_Name": "",
+                    "ParentHomeLang": "",
+                    "Second_Emergency_Contact_Phone1": "",
+                    "Relationship": "",
+                    "ParentPhone1": "",
+                    "Emergency_Contact_Phone2": "",
+                    "ParentLName": "",
+                    "LiabilityWaiver": false,
+                    "Ethnicity": "",
+                    "Second_Emergency_Contact_Relationship": "",
+                    "SchoolName": "",
+                    "PermissiontoCommuteAlone": "",
+                    "DCYFStuID": "",
+                    "MailingStreet": "",
+                    "Volunteer": "",
+                    "Grade": "",
+                    "Second_Emergency_Contact_Name": "",
+                    "DataReleaseWaiver": false,
+                    "MailingCity": "",
+                    "MediaReleaseWaiver": false,
+                    "ParentEnglishFluency": "",
+                    "Second_Emergency_Contact_Phone3": "",
+                    "MailingCountry": "",
+                    "MailingZip": 0,
+                    "ParentPhone3": "",
+                    "Emergency_Contact_Phone1": "",
+                    "ReducedPriceLunch": "",
+                    "Allergies": "",
+                    "Birthdate": "2020-01-02",
+                    "ContactRecordType": "",
+                    "Second_Emergency_Contact_Permission_to_Pickup_child": "",
+                    "Emergency_Contact_Relationship": "",
+                    "ParentEmail": "",
+                    "Emergency_Contact_Permission_to_Pickup_child": "",
+                    "ParentFName": "",
+                    "ParentPhone2": "",
+                    "LastName": fullNameSplit[0].trim(),
+                    "PersonalEmail": "",
+                    "MailingState": "",
+                    "Second_Emergency_Contact_Phone2": "",
+                    "MiddleName": "",
+                    "FirstName": fullNameSplit[1].trim(),
+                    "HomePhone": ""
                   };
                   Object.keys(districtToSalesforceExtraFieldMapping).map((item_2) => {
                     const currentFormValue = item.formValues[item_2];
                     if (!!currentFormValue) {
-
                       let valueToPass = currentFormValue;
-console.log(item_2);
                       if (item_2 === "Date of Birth") {
-
                         const dobSplit = currentFormValue.split("/").map((item) => item.trim());
-
-                        console.log("DOB HERE IS MORE");
-                        console.log(dobSplit);
-
                         if (dobSplit.length === 3) {
-                          console.log("DOB EXTRA IS HERE");
-
-                          valueToPass=`${dobSplit[2]}-${dobSplit[0]}-${dobSplit[1]}`
-
-
-                          console.log(dobSplit);
-
-
+                          const yearStr = `${dobSplit[2]}`;
+                          const monthStr = dobSplit[0].length === 1 ? `0${dobSplit[0]}` : `${dobSplit[0]}`;
+                          const dateOfMonthStr = dobSplit[1].length === 1 ? `0${dobSplit[1]}` : `${dobSplit[1]}`;
+                          valueToPass = `${yearStr}-${monthStr}-${dateOfMonthStr}`;
                         }
-
+                      }
+                      if (item_2 === "Zip") {
+                        valueToPass = parseInt(currentFormValue);
                       }
                       postRequestFields[districtToSalesforceExtraFieldMapping[item_2]] = valueToPass;
                     }
                   });
-                  salesforceEmptyFieldMappings.map((item_2) => {
-                    postRequestFields[item_2] = "";
-                  });
                   try {
-                    resolve_2(await runMulesoftAPIRequest_POST(generateMulesoftAPI_participant_post_new(), "api/contact", requestDate, postRequestFields));
+                    resolve_2(await runMulesoftAPIRequest_POST(generateMulesoftAPI_participant_post_new(), "api/contacts", requestDate, postRequestFields));
                   } catch (e) {
                     reject_2(e);
                   }
