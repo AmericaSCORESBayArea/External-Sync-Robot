b769151c86d302b775631d5211bd7a47d697ee0e
m <m@m.com>
2021-01-09 12:04:41 -0500

    removing unused mongodb commands rather than refactoring them to match the revised approach of establishing new connections for each mongodb command

--- node/src/mongo/getSingleDocument.js
+++
@@ -1,8 +0,0 @@
-import queryDocuments from "./query";
-
-const getSingleDocument = async (collectionName,query,fields) => {
-  const matchingDocs = !!fields ? await queryDocuments(collectionName, query, fields) : await queryDocuments(collectionName, query);
-  return matchingDocs.length === 1 ? matchingDocs[0] : {}
-};
-
-export default getSingleDocument;
