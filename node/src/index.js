3364c7743571b79001bdf14ed7cc12042e28f2ff
m <m@m.com>
2021-01-13 21:14:45 -0500

    updating command references

--- node/src/index.js
+++ node/src/index.js
@@ -6,10 +6,11 @@ import generateAvailableCommandsString from "./modules/generateAvailableCommands
 import get_contact_data from "./mulesoft_api/get_contact_data";
 import get_coach_data from "./mulesoft_api/get_coach_data";
 import get_coach_session_data from "./mulesoft_api/get_coach_session_data";
+import get_all_enrollments from "./mulesoft_api/get_all_enrollments";
 import list_participants_in_district_not_in_salesforce from "./mulesoft_api/list_participants_in_district_not_in_salesforce";
 import post_new_participants_to_salesforce from "./mulesoft_api/post_new_participants_to_salesforce";
 import post_missing_session_dates from "./mulesoft_api/post_missing_session_dates";
-import post_all_enrollments from "./mulesoft_api/post_all_enrollments";
+import post_missing_enrollments from "./mulesoft_api/post_missing_enrollments";
 import post_all_attendances from "./mulesoft_api/post_all_attendances";
 
 //todo "District 2" ->  set "Is youth a parent?" to "N"
@@ -34,6 +35,10 @@ const availableCommands = [
     entryPoint: get_coach_session_data
   },
   {
+    name: "get_all_enrollments",
+    entryPoint: get_all_enrollments
+  },
+  {
     name: "list_participants_in_district_not_in_salesforce",
     entryPoint: list_participants_in_district_not_in_salesforce
   },
@@ -46,8 +51,8 @@ const availableCommands = [
     entryPoint: post_missing_session_dates
   },
   {
-    name: "post_all_enrollments",
-    entryPoint: post_all_enrollments
+    name: "post_missing_enrollments",
+    entryPoint: post_missing_enrollments
   },
   {
     name: "post_all_attendances",
@@ -55,29 +60,25 @@ const availableCommands = [
   }
 ];
 
-//main entry point
 const main = () => {
   console.log(`Started : ${new Date()}`);
   console.log(`....CLI Arguments : ${cliArguments.join(", ")}`);
   if (cliArguments.length > 0) {
-    const requestedCommand = cliArguments[0];
-    const matchingEntryPoint = availableCommands.filter((item) => !!item.name && item.name === requestedCommand);
-    if (matchingEntryPoint.length > 0) {
-      new Promise(async () => {
-        await matchingEntryPoint[0].entryPoint(cliArguments);
-      }).then((res, err) => {
-        if (!!err) {
-          console.error("unknown error--3 in main");
+    return new Promise(async () => {
+      const requestedCommand = cliArguments[0];
+      const matchingEntryPoint = availableCommands.filter((item) => !!item.name && item.name === requestedCommand);
+      if (matchingEntryPoint.length > 0) {
+        try {
+          await matchingEntryPoint[0].entryPoint(cliArguments);
+        } catch (err) {
+          console.error("unknown error in main---1");
           console.error(err);
         }
-      }).catch((err) => {
-        console.error("unknown error--4 in main");
-        console.error(err);
-      }).then(() => {});
-    } else {
-      console.log(`no matching command for the request : ${requestedCommand}`);
-      console.log(generateAvailableCommandsString(availableCommands));
-    }
+      } else {
+        console.log(`no matching command for the request : ${requestedCommand}`);
+        console.log(generateAvailableCommandsString(availableCommands));
+      }
+    })
   } else {
     console.log("must pass in at least one argument");
     console.log(generateAvailableCommandsString(availableCommands));
@@ -85,4 +86,17 @@ const main = () => {
 };
 
 //main entry point
-main();
+main().then((res, err) => {
+  if (!!err) {
+    console.error("unknown error in main---2");
+    console.error(err);
+  }
+  if (!!res) {
+    console.log(res);
+  }
+}).catch((err) => {
+  console.error("unknown error in main---3");
+  console.error(err);
+}).then(() => {
+  console.log(`Done - ${new Date()}`);
+});
