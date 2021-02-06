73037980c36344ba71caadd49e751ada18b20beb
m <m@m.com>
2021-02-06 15:30:33 -0500

    changing to string output to display full values

--- api/src/selenium/setBrowserTimeouts.js
+++ api/src/selenium/setBrowserTimeouts.js
@@ -4,7 +4,7 @@ const script_timeout_milliseconds = 60*60*1000*5;   //5 hours
 const timeoutObj = { implicit: implicit_timeout_milliseconds, pageLoad:pageLoad_timeout_milliseconds, script: script_timeout_milliseconds };
 
 const setBrowserTimeouts = (browser) => {
-  console.log(`setting browser timeouts to : ${timeoutObj}`);
+  console.log(`setting browser timeouts to : ${JSON.stringify(timeoutObj)}`);
   return new Promise(async (resolve) => {
     try {
       await browser.manage().setTimeouts(timeoutObj)
