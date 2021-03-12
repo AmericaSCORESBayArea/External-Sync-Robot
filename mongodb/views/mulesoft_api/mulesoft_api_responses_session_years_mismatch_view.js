3657798628e7f48b8a1d2b8c7e8cff0675c34dcd
matthewjerome <24796700+matthewjerome@users.noreply.github.com>
2021-03-12 16:36:56 -0500

    Implement Identifying Mismatched Session Year + Team Season Year (#78)
    * generating a view that takes the input session dates and team season name date and compares to find any mismatches
    
    * reworking the logic for splitting team season names to extract out the year which takes into account team season names containing dashes
    
    Co-authored-by: m <m@m.com>

---
+++ mongodb/views/mulesoft_api/mulesoft_api_responses_session_years_mismatch_view.js
@@ -0,0 +1,105 @@
+db.mulesoft_api_responses_session_years_mismatch_view.drop();
+db.createView("mulesoft_api_responses_session_years_mismatch_view","mulesoft_api_responses_session_view",
+
+  // Pipeline
+  [
+    // Stage 1
+    {
+      $project: {
+        "_id" : 1.0,
+        "sessionId" : 1.0,
+        "teamSeasonId" : 1.0,
+        "sessionDate" : 1.0,
+        "teamSeasonName" : "$TeamSeasonName",
+        "sessionYear" : {
+          "$toString" : {
+            "$year" : "$sessionDate"
+          }
+        },
+        "TeamSeasonNameSplit" : {
+          "$split" : [
+            "$TeamSeasonName",
+            "-"
+          ]
+        }
+      }
+    },
+
+    // Stage 2
+    {
+      $project: {
+        "_id" : 1.0,
+        "sessionId" : 1.0,
+        "teamSeasonId" : 1.0,
+        "sessionYear" : 1.0,
+        "teamSeasonName" : 1.0,
+        "sessionDate" : 1.0,
+        "teamSeasonNameYearSplit" : {
+          "$split" : [
+            {
+              "$arrayElemAt" : [
+                "$TeamSeasonNameSplit",
+                -1.0
+              ]
+            },
+            " "
+          ]
+        }
+      }
+    },
+
+    // Stage 3
+    {
+      $project: {
+        "_id" : 1.0,
+        "sessionId" : 1.0,
+        "teamSeasonId" : 1.0,
+        "sessionYear" : 1.0,
+        "teamSeasonName" : 1.0,
+        "sessionDate" : 1.0,
+        "teamSeasonNameYear" : {
+          "$trim" : {
+            "input" : {
+              "$arrayElemAt" : [
+                "$teamSeasonNameYearSplit",
+                0.0
+              ]
+            }
+          }
+        }
+      }
+    },
+
+    // Stage 4
+    {
+      $project: {
+        "_id" : 1.0,
+        "sessionId" : 1.0,
+        "teamSeasonId" : 1.0,
+        "sessionYear" : 1.0,
+        "teamSeasonName" : 1.0,
+        "sessionDate" : 1.0,
+        "teamSeasonNameYear" : 1.0,
+        "yearsMatch" : {
+          "$cond" : [
+            {
+              "$eq" : [
+                "$sessionYear",
+                "$teamSeasonNameYear"
+              ]
+            },
+            true,
+            false
+          ]
+        }
+      }
+    },
+
+    // Stage 5
+    {
+      $match: {
+        "yearsMatch" : false
+      }
+    },
+  ],
+)
