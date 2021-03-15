f8e7c972999a082ab1ec5a02c8cdda0ae8bdd1c7
matthewjerome <24796700+matthewjerome@users.noreply.github.com>
2021-03-15 14:18:56 -0400

    creating a field summary view for new participants (#87)
    Co-authored-by: m <m@m.com>

---
+++ mongodb/views/mulesoft_api/mulesoft_api_responses_participants_not_found_view_field_view.js
@@ -0,0 +1,57 @@
+db.mulesoft_api_responses_participants_not_found_view_field_view.drop();
+db.createView("mulesoft_api_responses_participants_not_found_view_field_view","mulesoft_api_responses_participants_not_found_view",
+
+  // Pipeline
+  [
+    // Stage 1
+    {
+      $lookup: {
+        "from" : "mulesoft_api_responses_participants_view",
+        "localField" : "participantId",
+        "foreignField" : "participantId_district",
+        "as" : "matchingMulesoftResponse"
+      }
+    },
+
+    // Stage 2
+    {
+      $unwind: {
+        "path" : "$matchingMulesoftResponse",
+        "includeArrayIndex" : "matchingMulesoftResponseIndex",
+        "preserveNullAndEmptyArrays" : true
+      }
+    },
+
+    // Stage 3
+    {
+      $match: {
+        "matchingMulesoftResponseIndex" : null
+      }
+    },
+
+    // Stage 4
+    {
+      $group: {
+        "_id" : "$_id",
+        "ActivityName" : {
+          "$first" : "$ActivityName"
+        },
+        "participant" : {
+          "$first" : "$participant"
+        },
+        "status" : {
+          "$first" : "$status"
+        },
+        "formValues" : {
+          "$first" : "$formValues"
+        },
+        "district" : {
+          "$first" : "$district"
+        },
+        "participantId" : {
+          "$first" : "$participantId"
+        }
+      }
+    },
+  ]
+);
