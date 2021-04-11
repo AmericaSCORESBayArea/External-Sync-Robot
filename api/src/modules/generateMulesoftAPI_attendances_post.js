ba719f724de2b3cfcf4a4fc9c761f3cf6ada2211
m <m@m.com>
2021-04-11 16:18:32 -0400

    updating import references to relocated subfolder

--- api/src/modules/generateMulesoftAPI_attendances_post.js
+++ api/src/modules/generateMulesoftAPI_attendances_post.js
@@ -1,3 +1,3 @@
-import getConfigurationValueByKey from "./getConfigurationValueByKey";
+import getConfigurationValueByKey from "./dot-env-configuration/getConfigurationValueByKey";
 const generateMulesoftAPIEndpoint_attendances_post = () => `${getConfigurationValueByKey("MULESOFT_API_ROOT_URL")}/attendances`;
 export default generateMulesoftAPIEndpoint_attendances_post;
--- api/src/modules/generateMulesoftAPI_coach_coachIdAndDate.js
+++ api/src/modules/generateMulesoftAPI_coach_coachIdAndDate.js
@@ -1,3 +1,3 @@
-import getConfigurationValueByKey from "./getConfigurationValueByKey";
+import getConfigurationValueByKey from "./dot-env-configuration/getConfigurationValueByKey";
 const generateMulesoftAPIEndpoint_coach_coachIdAndDate = (coachId,dateFilter) => `${getConfigurationValueByKey("MULESOFT_API_ROOT_URL")}/coach/${coachId}/all?date=${dateFilter}`;
 export default generateMulesoftAPIEndpoint_coach_coachIdAndDate;
