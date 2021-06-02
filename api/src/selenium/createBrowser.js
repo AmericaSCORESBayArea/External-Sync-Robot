c81b809bda1cec1a18b15fb4a0791dd9cd492281
matthewjerome <24796700+matthewjerome@users.noreply.github.com>
2021-06-02 12:28:57 -0400

    adding setAcceptInsecureCerts to options generator (#104)
    Co-authored-by: m <m@m.com>

--- api/src/selenium/createBrowser.js
+++ api/src/selenium/createBrowser.js
@@ -1,12 +1,16 @@
 import {Builder} from 'selenium-webdriver';
 import firefox from 'selenium-webdriver/firefox';
 
-const createBrowser = (options) => {
+const createOptions = () => {
+  const options = {};
+  return new firefox.Options(options).setAcceptInsecureCerts(true);
+};
+
+const createBrowser = () => {
   console.log(`creating a browser instance...`);
   return new Promise(async (resolve, reject) => {
     try {
-      let options = new firefox.Options(options);
-      resolve(await new Builder().forBrowser("firefox").setFirefoxOptions(options).build());
+      resolve(await new Builder().forBrowser("firefox").setFirefoxOptions(createOptions()).build());
     } catch(e) {
       console.error(`error creating selenium browser object`);
       console.error(e);
