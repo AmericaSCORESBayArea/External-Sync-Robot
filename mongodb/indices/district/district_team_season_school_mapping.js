cb3fdefaf0456fb145979f5092d5c5f0cabdc766
m <m@m.com>
2021-12-06 19:31:52 -0500

    creating new collection with indices for school names

---
+++ mongodb/indices/district/district_team_season_school_mapping.js
@@ -0,0 +1,3 @@
+db.district_team_season_school_mapping.createIndex({"districtSystemTeamName":1});
+db.district_team_season_school_mapping.createIndex({"schoolName":1});
+db.district_team_season_school_mapping.createIndex({"district":1});
