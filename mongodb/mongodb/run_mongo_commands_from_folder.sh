0074ba84257b56d95acf51dad2e757cfd9e4f213
matthewjerome <24796700+matthewjerome@users.noreply.github.com>
2022-02-27 14:57:59 -0500

    Dockerize Node API (#197)
    * updated package lock
    
    * removing unnecessary script
    
    * removing unnecessary script
    
    * relocating scripts
    
    * remove unnecessary config
    
    * update relative source path
    
    * update build path folder reference
    
    * add dockerignore
    
    * relocating script
    
    * relocating
    
    * removing local mongodb folder
    
    * updating build script path reference
    
    * successfully building node app from source in docker container
    
    * successfully starting node app
    
    Co-authored-by: m <m@m.com>

--- mongodb/mongodb/run_mongo_commands_from_folder.sh
+++ mongodb/mongodb/run_mongo_commands_from_folder.sh
@@ -3,10 +3,7 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P )
 
 # shellcheck disable=SC1090
-source "$PARENT_PATH"/../../api/.env;
-
-# shellcheck disable=SC1090
-source "$PARENT_PATH"/../datetime/current_date_time.sh;
+source "$PARENT_PATH"/../../.env;
 
 runMongoCommandsFromFolder() {
 
