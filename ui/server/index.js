59ea4d0eff4b46cedff0d858b1aee46b5fe7f650
m <m@m.com>
2022-03-02 14:13:24 -0500

    creating a ui server from the React scripts build output

---
+++ ui/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bp = require('body-parser');
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log("%c Sync Robot React UI Server running", "color: green");
+});
