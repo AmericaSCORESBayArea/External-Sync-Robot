ca1be3726780a22e8250e2da2464186fb1a6e6e7
m <m@m.com>
2022-03-08 15:12:55 -0500

    add method to insert many docs to mongodb

---
+++ api/src/mongo/insertMany.js
@@ -0,0 +1,20 @@
+import getConfigurationValueByKey from "../modules/dot-env-configuration/getConfigurationValueByKey";
+import generateMongoDBConnectionURL from "./generateMongoDBConnectionURL";
+const MongoClient = require('mongodb').MongoClient;
+
+const insertManyDocuments = (collectionName,documents) => {
+  try {
+    return new Promise(async (resolve, reject) => {
+      await MongoClient.connect(generateMongoDBConnectionURL(), {useUnifiedTopology: true}, function (err, client) {
+        const db = client.db(getConfigurationValueByKey("MONGO_DATABASE"));
+        const collection = db.collection(collectionName);
+        collection.insertMany(documents, (err, result) => !!err ? reject(err) : resolve(result.insertedId));
+      });
+    });
+  } catch (err) {
+    console.error(`error inserting (insertManyDocuments) into mongo server ${collectionName}`,err);
+    return null;
+  }
+};
+
+export default insertManyDocuments;
