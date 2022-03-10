b18d16042c318a714fc600747179cec0bde194d5
m <m@m.com>
2022-03-10 16:09:21 -0500

    updating default team season name mappings

--- api/mongodb/collections/district_team_season_name_mapping.js
+++ api/mongodb/collections/district_team_season_name_mapping.js
@@ -1,404 +1,49 @@
 if (db.district_team_season_name_mapping.find().count() === 0) {
   db.district_team_season_name_mapping.insertMany([
-    {
-      "districtSystemTeamName": "Bret Harte 2020-2021",
-      "teamSeasonName": "Bret Harte Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Bret Harte 2020-2021",
-      "teamSeasonName": "Bret Harte Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Cobb 2020-2021",
-      "teamSeasonName": "Cobb-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Cobb 2020-2021",
-      "teamSeasonName": "Cobb-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "ER Taylor  Select 2020-2021",
-      "teamSeasonName": "ER Taylor Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "ER Taylor  Select 2020-2021",
-      "teamSeasonName": "ER Taylor Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "ER Taylor Juniors 2020-2021",
-      "teamSeasonName": "ER Taylor Juniors-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "ER Taylor Juniors 2020-2021",
-      "teamSeasonName": "ER Taylor Juniors-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Guadalupe 2020-2021",
-      "teamSeasonName": "Guadalupe-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Guadalupe 2020-2021",
-      "teamSeasonName": "Guadalupe-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Hillcrest 2020-2021",
-      "teamSeasonName": "Hillcrest-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Hillcrest 2020-2021",
-      "teamSeasonName": "Hillcrest-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "IHDC 2020-2021",
-      "teamSeasonName": "IHDC Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "IHDC 2020-2021",
-      "teamSeasonName": "IHDC Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "J Serra Select 2020-2021",
-      "teamSeasonName": "J Serra Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "J Serra Select 2020-2021",
-      "teamSeasonName": "J Serra Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "KROC 2020-2021",
-      "teamSeasonName": "Salvation Army KROC-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "KROC 2020-2021",
-      "teamSeasonName": "Salvation Army KROC-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Lakeshore Select 2020-2021",
-      "teamSeasonName": "Lakeshore Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Lakeshore Select 2020-2021",
-      "teamSeasonName": "Lakeshore Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "MLK Middle School 2020-2021",
-      "teamSeasonName": "MLK Coed-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "MLK Middle School 2020-2021",
-      "teamSeasonName": "MLK Coed-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Marshall Select 2020-2021",
-      "teamSeasonName": "Marshall Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Marshall Select 2020-2021",
-      "teamSeasonName": "Marshall Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission Education Center Juniors 2020-2021",
-      "teamSeasonName": "Mission Education Center Juniors-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission Education Center Juniors 2020-2021",
-      "teamSeasonName": "Mission Education Center Juniors-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission Education Center Select 2020-2021",
-      "teamSeasonName": "Mission Education Center Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission Education Center Select 2020-2021",
-      "teamSeasonName": "Mission Education Center Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission YMCA Learning Hub Juniors 2020-2021",
-      "teamSeasonName": "Mission YMCA Learning Hub Juniors-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission YMCA Learning Hub Juniors 2020-2021",
-      "teamSeasonName": "Mission YMCA Learning Hub Juniors-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission YMCA Learning Hub Select 2020-2021",
-      "teamSeasonName": "Mission YMCA Learning Hub Select-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Mission YMCA Learning Hub Select 2020-2021",
-      "teamSeasonName": "Mission YMCA Learning Hub Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Moscone 2020-2021",
-      "teamSeasonName": "Moscone-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Moscone 2020-2021",
-      "teamSeasonName": "Moscone-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Paul Revere Middle 2020-2021",
-      "teamSeasonName": "Paul Revere Middle-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Paul Revere Middle 2020-2021",
-      "teamSeasonName": "Paul Revere Middle-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Redding 2020-2021",
-      "teamSeasonName": "Redding-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Redding 2020-2021",
-      "teamSeasonName": "Redding-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Sanchez 2020-2021",
-      "teamSeasonName": "Sanchez-2020 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Sanchez 2020-2021",
-      "teamSeasonName": "Sanchez-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Encompass Coed",
-      "teamSeasonName": "EnCompass COED-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Encompass Coed",
-      "teamSeasonName": "EnCompass COED-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Howard Coed",
-      "teamSeasonName": "Howard Coed-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Howard Coed",
-      "teamSeasonName": "Howard Coed-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 ICS/TCN Coed",
-      "teamSeasonName": "ICS/TCN COED-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 ICS/TCN Coed",
-      "teamSeasonName": "ICS/TCN COED-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 KDA/Esp Boys",
-      "teamSeasonName": "KDA/Esp Boys-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 KDA/Esp Boys",
-      "teamSeasonName": "KDA/Esp Boys-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 KDA/Esp Girls",
-      "teamSeasonName": "KDA/Esp Girls-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 KDA/Esp Girls",
-      "teamSeasonName": "KDA/Esp Girls-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Learning Without Limits Coed",
-      "teamSeasonName": "Learning Without Limits COED-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Learning Without Limits Coed",
-      "teamSeasonName": "Learning Without Limits COED-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 MLK Coed",
-      "teamSeasonName": "MLK Lafayette Coed-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 MLK Coed",
-      "teamSeasonName": "MLK Lafayette Coed-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Madison Primary Coed",
-      "teamSeasonName": "Madison Coed-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Madison Primary Coed",
-      "teamSeasonName": "Madison Coed-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Manzanita Community School",
-      "teamSeasonName": "Manzanita Community-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Manzanita SEED Coed",
-      "teamSeasonName": "Manzanita SEED-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Manzanita SEED Coed",
-      "teamSeasonName": "Manzanita SEED-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Sankofa United Coed",
-      "teamSeasonName": "Sankofa Coed-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-20201 Sankofa United Coed",
-      "teamSeasonName": "Sankofa Coed-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 ASCEND Coed",
-      "teamSeasonName": "ASCEND COED-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 ASCEND Coed",
-      "teamSeasonName": "ASCEND COED-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Brete Harte",
-      "teamSeasonName": "Bret Harte Oakland COED-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Elmhurst Boys",
-      "teamSeasonName": "Elmhurst Boys-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Elmhurst Boys",
-      "teamSeasonName": "Elmhurst Boys-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Life Academy 6th",
-      "teamSeasonName": "Life Academy 6th-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Life Academy 6th",
-      "teamSeasonName": "Life Academy 6th-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Life Academy 7th",
-      "teamSeasonName": "Life Academy 7th-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Life Academy 7th",
-      "teamSeasonName": "Life Academy 7th-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Life Academy 8th",
-      "teamSeasonName": "Life Academy 8th-2020 Fall",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Life Academy 8th",
-      "teamSeasonName": "Life Academy 8th-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 CCPA Boys",
-      "teamSeasonName": "CCPA Boys-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "2020-2021 Manzanita Community 2nd Grade",
-      "teamSeasonName": "Manzanita Community-2021 Spring",
-      "district": "district_2"
-    },
-    {
-      "districtSystemTeamName": "Bessie Carmichael 2020-2021",
-      "teamSeasonName": "Bessie Carmichael-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Bryant-Flynn Select 2020-2021",
-      "teamSeasonName": "Bryant-Flynn Select-2021 Fall",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Bryant-Flynn Select 2020-2021",
-      "teamSeasonName": "Bryant-Flynn Select-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "Paul Revere Select #2 2020-2021",
-      "teamSeasonName": "Paul Revere Middle-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "TECA-Alvarado 2020-2021",
-      "teamSeasonName": "TECA-Alvarado-2021 Spring",
-      "district": "district_1"
-    },
-    {
-      "districtSystemTeamName": "John Muir 2020-2021",
-      "teamSeasonName": "John Muir-2021 Spring",
-      "district": "district_1"
-    }
+    {"district":"district_1","districtSystemTeamName":"Bessie Carmichael Juniors 2021-2022","teamSeasonName":"Bessie Carmichael Juniors-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Bessie Carmichael Select 2021-2022","teamSeasonName":"Bessie Carmichael Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Bret Harte Juniors 2021-2022","teamSeasonName":"Bret Harte Juniors-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Bret Harte Select 2021-2022","teamSeasonName":"Bret Harte Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Bryant Select 2021-2022","teamSeasonName":"Bryant Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Cleveland Select 2021-2022","teamSeasonName":"Cleveland Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"ER Taylor Juniors 2021-2022","teamSeasonName":"ER Taylor Juniors-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"ER Taylor Select 2021-2022","teamSeasonName":"ER Taylor Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Everett 2021-2022","teamSeasonName":"Everett Middle School-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Francisco Middle School 2021-2022","teamSeasonName":"Francisco Boys-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Hillcrest Juniors 2021-2022","teamSeasonName":"Hillcrest Juniors-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Hillcrest Select 2021-2022","teamSeasonName":"Bessie Carmichael Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"IHDC Select 2021-2022","teamSeasonName":"IHDC Select-2021 Fall"},
+
+    {"district":"district_1","districtSystemTeamName":"John Muir 2021-2022","teamSeasonName":"John Muir-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Joseph Lee Rec Center 2021-2022","teamSeasonName":"Joseph Lee Rec Center-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"MLK Coed 2021-2022","teamSeasonName":"MLK Coed-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Paul Revere Juniors 2021-2022","teamSeasonName":"Paul Revere Juniors-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Paul Revere Middle 2021-2022","teamSeasonName":"Paul Revere Middle-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Paul Revere Select 2021-2022","teamSeasonName":"Paul Revere Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Salvation Army KROC 2021-2022","teamSeasonName":"Salvation Army KROC-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Sanchez Juniors 2021-2022","teamSeasonName":"Sanchez Juniors-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Sanchez Select 2021-2022","teamSeasonName":"Sanchez Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"TECA - Program Site Juniors 2021-2022","teamSeasonName":"TECA Juniors"},
+    {"district":"district_1","districtSystemTeamName":"TECA - Program Site Select 2021-2022","teamSeasonName":"TECA SELECT-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"TECA MS Boys 2021-2022","teamSeasonName":"TECA MS Boys-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Up on Top 2021-2022","teamSeasonName":"Up on Top - Program Site Select-2021 Fall"},
+    {"district":"district_1","districtSystemTeamName":"Bessie Carmichael Juniors 2021-2022","teamSeasonName":"Bessie Carmichael Juniors-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Bessie Carmichael Select 2021-2022","teamSeasonName":"Bessie Carmichael Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Bret Harte Juniors 2021-2022","teamSeasonName":"Bret Harte Juniors-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Bret Harte Select 2021-2022","teamSeasonName":"Bret Harte Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Bryant Select 2021-2022","teamSeasonName":"Bryant Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Cleveland Select 2021-2022","teamSeasonName":"Cleveland Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"ER Taylor Juniors 2021-2022","teamSeasonName":"ER Taylor Juniors-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"ER Taylor Select 2021-2022","teamSeasonName":"ER Taylor Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Hillcrest Select 2021-2022","teamSeasonName":"Hillcrest Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"IHDC Select 2021-2022","teamSeasonName":"IHDC Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"John Muir 2021-2022","teamSeasonName":"John Muir-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"MLK Coed 2021-2022","teamSeasonName":"MLK Coed-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Paul Revere Juniors 2021-2022","teamSeasonName":"Paul Revere Juniors-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Paul Revere Middle 2021-2022","teamSeasonName":"Paul Revere Middle-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Paul Revere Select 2021-2022","teamSeasonName":"Paul Revere Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Salvation Army KROC 2021-2022","teamSeasonName":"Salvation Army KROC-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Sanchez Select 2021-2022","teamSeasonName":"Sanchez Select-2022 Spring"},
+    {"district":"district_1","districtSystemTeamName":"Up on Top 2021-2022","teamSeasonName":"Up on Top - Program Site Select-2022 Spring"}
   ]);
 }
