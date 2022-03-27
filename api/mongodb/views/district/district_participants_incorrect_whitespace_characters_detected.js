43ce94f8a648e1f65a534b41b76cecf1b6d9963f
m <m@m.com>
2022-03-27 11:48:45 -0400

    create detect incorrect whitespace view

---
+++ api/mongodb/views/district/district_participants_incorrect_whitespace_characters_detected.js
@@ -0,0 +1,56 @@
+db.district_participants_incorrect_whitespace_characters_detected.drop();
+db.createView("district_participants_incorrect_whitespace_characters_detected","district_participants",
+
+  // Pipeline
+  [
+    // Stage 1
+    {
+      $match: {
+        "$or" : [
+          {
+            "participant.name" : {
+              "$regex" : /^( )/
+            }
+          },
+          {
+            "participant.name" : {
+              "$regex" : /( )$/
+            }
+          },
+          {
+            "participant.name" : {
+              "$regex" : /.+( ),.+/
+            }
+          },
+          {
+            "participant.name" : {
+              "$regex" : /.+,( )( )/
+            }
+          },
+          {
+            "participant.name" : {
+              "$regex" : /( )( )/
+            }
+          }
+        ]
+      }
+    },
+
+    // Stage 2
+    {
+      $project: {
+        "_id" : 1.0,
+        "district" : 1.0,
+        "name" : "$participant.name"
+      }
+    },
+
+    // Stage 3
+    {
+      $sort: {
+        "district" : 1.0,
+        "name" : 1.0
+      }
+    }
+  ]
+);
